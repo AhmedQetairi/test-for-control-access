e08f2b279d1341e64ec9e8344e74b9caad1279f3
AhmedQetairi <ahmad1996_2014@hotmail.com>
2022-11-17 10:19:04 +0000

    create new module

---
+++ partners_limit/models/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+
+from odoo import models
+
+
+class partnerslimit(models.Model):
+        _name = 'partners.limit'
+        _description = "Partners Limitation"
